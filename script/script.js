56da4c8e7f5c82ae485a774c5563c5a8b66e2835
GlitchBanger <89722364+GlitchBanger@users.noreply.github.com>
2022-04-09 18:26:54 +0530

    created js file and made changes in track

---
+++ script/script.js
@@ -0,0 +1,6 @@
+let entrepreneur = {
+    title: 'Entrepreneurship',
+    timings: '11:00am',
+    keynote: 'Dr. Jacob Crasta',
+    description: 'Entrepreneurship education is a key factor in developing entrepreneurial qualities in students. Entrepreneurship also has a potential of providing solution to the sustainable development challenges. It can be fostered through transforming the higher education systems and institutions at large. By transformation, we mean to create innovative and entrepreneurial learning experiences by developing a collaborative ecosystem of institutions. Entrepreneurship development could be made through a collective approach of the qualified individuals through these collaborations. Youth entrepreneurship has been recognised as one of the solutions which can address the unprecedented global youth unemployment challenge.'
+};
